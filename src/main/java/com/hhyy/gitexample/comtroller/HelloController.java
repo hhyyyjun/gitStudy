8a0cc3e4d31026aba516853595e2b1599e1a342b
hhyyyjun <jun950617@naver.com>
2023-06-19 10:31:52 +0900

    Initial commit

---
+++ src/main/java/com/hhyy/gitexample/comtroller/HelloController.java
@@ -0,0 +1,21 @@
+package com.hhyy.gitexample.comtroller;
+
+/**
+ * packageName    : com.hhyy.gitexample.comtroller
+ * fileName       : HelloController
+ * author         : hj
+ * date           : 2023-06-19
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-06-19        hj       최초 생성
+ */
+public class HelloController {
+    
+    public String hi(){
+        System.out.println("안녕 테스트");
+        return "dd";
+    }
+    
+}
